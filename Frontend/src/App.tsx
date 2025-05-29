9a740c4c2f5be5f01ab452d92d5ee24255ae6f4f
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-05-29 15:55:55 +0530

    added tailwind and shadcn, configuration resolved

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div className="">Hello</div>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
