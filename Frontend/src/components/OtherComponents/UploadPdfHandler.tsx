e8906da4ec0094f00461a4d019294ec4330b3183
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-05-30 16:00:25 +0530

    fixed zod issue and created Schema for file upload.

--- Frontend/src/components/OtherComponents/UploadPdfHandler.tsx
+++ Frontend/src/components/OtherComponents/UploadPdfHandler.tsx
@@ -1,10 +1,65 @@
 import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
+import { DOCUMENT_SCHEMA } from "@/utils/schema/DOCUMENT_SCHEMA";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+interface ErrorType {
+  doc_upload?: string;
+}
 
 const UploadPdfHandler = () => {
   const [uploadedFile, setUploadedFile] = useState<File | undefined>();
+  const [error, setError] = useState<ErrorType>({});
 
-  return <Input type="file" />;
+  const form = useForm<z.infer<typeof DOCUMENT_SCHEMA>>({
+    resolver: zodResolver(DOCUMENT_SCHEMA),
+    defaultValues: {
+      file: undefined,
+    },
+  });
+
+  const handleDocChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
+  function onSubmit(values: z.infer<typeof DOCUMENT_SCHEMA>) {
+    console.log(values);
+  }
+
+  return  (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default UploadPdfHandler;
--- Frontend/src/utils/schema/DOCUMENT_SCHEMA.ts
+++ Frontend/src/utils/schema/DOCUMENT_SCHEMA.ts
@@ -2,11 +2,13 @@ import { z } from "zod";
 
 const fileSizeLimit = 10 * 1024 * 1024;
 
-export const DOCUMENT_SCHEMA = z
-  .instanceof(File)
-  .refine((file) => ["application/pdf"].includes(file.type), {
-    message: "Invalid document file type",
-  })
-  .refine((file) => file.size <= fileSizeLimit, {
-    message: "File size should not exceed 5MB",
-  });
+export const DOCUMENT_SCHEMA = z.object({
+  file: z
+    .instanceof(File)
+    .refine((file) => ["application/pdf"].includes(file.type), {
+      message: "Invalid document file type",
+    })
+    .refine((file) => file.size <= fileSizeLimit, {
+      message: "File size should not exceed 5MB",
+    }),
+});
