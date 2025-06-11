fcaac4fddb7798a2886a1ffce83ee7316e98b0f6
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-06-11 15:50:01 +0530

    added multer to upload file

--- Backend/routes/uploadRoute.js
+++ Backend/routes/uploadRoute.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const PDFParser = require("pdf-parse");
+const fs = require("fs");
+
+const uploadDirectory = "./uploads";
+const multer = require("multer");
+const path = require("path");
+const mammoth = require("mammoth");
+const pdf = require("html-pdf");
+
 const uploadRouter = express.Router();
 
-uploadRouter.get("/upload", (req, res, next) => {
- 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "UploadedFiles/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
+const upload = multer({ storage });
 
+uploadRouter.get("/upload", (req, res, next) => {});
 
 module.exports = uploadRouter;
